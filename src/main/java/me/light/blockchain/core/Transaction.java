1449e2062985023f7d0bc6507a1bc2153a0edcfd
lighthao <haoweiguang1986@gmail.com>
2018-10-09 16:11:10 +0800

    添加验证部分代码

--- src/main/java/me/light/blockchain/core/Transaction.java
+++ src/main/java/me/light/blockchain/core/Transaction.java
@@ -9,10 +9,18 @@ import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.bouncycastle.jcajce.provider.asymmetric.ec.BCECPrivateKey;
+import org.bouncycastle.jce.ECNamedCurveTable;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.jce.spec.ECParameterSpec;
+import org.bouncycastle.jce.spec.ECPublicKeySpec;
+import org.bouncycastle.math.ec.ECPoint;
 
+import java.math.BigInteger;
+import java.security.KeyFactory;
+import java.security.PublicKey;
 import java.security.Security;
 import java.security.Signature;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.Map;
 
@@ -40,6 +48,19 @@ public class Transaction {
 	 * 交易输出
 	 */
 	private TransactionOutput[] outputs;
+
+	/**
+	 * 计算交易信息的Hash值
+	 *
+	 * @return
+	 */
+	public byte[] hash() {
+		// 使用序列化的方式对Transaction对象进行深度复制
+		byte[] serializeBytes = SerializeUtils.serialize(this);
+		Transaction copyTx = (Transaction) SerializeUtils.deserialize(serializeBytes);
+		copyTx.setTransactionId(new byte[]{});
+		return DigestUtils.sha256(SerializeUtils.serialize(copyTx));
+	}
 
 
 	public byte[] getTransactionId() {
@@ -197,13 +218,85 @@ public class Transaction {
 		ecdsaSign.initSign(privateKey);
 
 		for (int i = 0; i < transaction4Sign.getInputs().length; i++) {
-			TransactionInput input = transaction4Sign.getInputs()[i];
+			TransactionInput input4Sign = transaction4Sign.getInputs()[i];
 			//获取交易输入TransactionId对应的交易数据
-			Transaction transaction = transactions.get(Hex.encodeHexString(input.getTransactionId()));
+			Transaction transaction = transactions.get(Hex.encodeHexString(input4Sign.getTransactionId()));
 			//获取交易输入所对应的上一笔交易中的交易输出
+			TransactionOutput output4Sgin = transaction.getOutputs()[input4Sign.getTransactionOutputIndex()];
+			input4Sign.setPublicKey(output4Sgin.getPublicKeyHash());
+			input4Sign.setSignature(null);
+			//得到签名的数据，即交易ID
+			transaction4Sign.setTransactionId(transaction4Sign.hash());
+			input4Sign.setPublicKey(null);
+
+			//对整个交易信息进行签名，即对交易ID进行签名
+			ecdsaSign.update(transaction4Sign.getTransactionId());
+			byte[] signature = ecdsaSign.sign();
+
+
+			// 将整个交易数据的签名赋值给交易输入，因为交易输入需要包含整个交易信息的签名
+			// 注意是将得到的签名赋值给原交易信息中的交易输入
+			this.getInputs()[i].setSignature(signature);
 		}
 
 
+	}
+
+	/**
+	 * 验证交易信息
+	 *
+	 * @param prevTxMap 前面多笔交易集合
+	 * @return
+	 */
+	public boolean verify(Map<String, Transaction> prevTxMap) throws Exception {
+		//coinbase 交易信息不需要签名，也就无需验证
+		if (this.isCoinBase()) {
+			return true;
+		}
+
+		// 再次验证一下交易信息中的交易输入是否正确，也就是能否查找对应的交易数据
+		for (TransactionInput txInput : this.getInputs()) {
+			if (prevTxMap.get(Hex.encodeHexString(txInput.getTransactionId())) == null) {
+				throw new Exception("ERROR: Previous transaction is not correct");
+			}
+		}
+
+		// 创建用于签名验证的交易信息的副本
+		Transaction txCopy = this.trimmedCopy();
+
+		Security.addProvider(new BouncyCastleProvider());
+		ECParameterSpec ecParameters = ECNamedCurveTable.getParameterSpec("secp256k1");
+		KeyFactory keyFactory = KeyFactory.getInstance("ECDSA", BouncyCastleProvider.PROVIDER_NAME);
+		Signature ecdsaVerify = Signature.getInstance("SHA256withECDSA", BouncyCastleProvider.PROVIDER_NAME);
+
+		for (int i = 0; i < this.getInputs().length; i++) {
+			TransactionInput txInput = this.getInputs()[i];
+			// 获取交易输入TxID对应的交易数据
+			Transaction prevTx = prevTxMap.get(Hex.encodeHexString(txInput.getTransactionId()));
+			// 获取交易输入所对应的上一笔交易中的交易输出
+			TransactionOutput prevTxOutput = prevTx.getOutputs()[txInput.getTransactionOutputIndex()];
+
+			TransactionInput txInputCopy = txCopy.getInputs()[i];
+			txInputCopy.setSignature(null);
+			txInputCopy.setPublicKey(prevTxOutput.getPublicKeyHash());
+			// 得到要签名的数据，即交易ID
+			txCopy.setTransactionId(txCopy.hash());
+			txInputCopy.setPublicKey(null);
+
+			// 使用椭圆曲线 x,y 点去生成公钥Key
+			BigInteger x = new BigInteger(1, Arrays.copyOfRange(txInput.getPublicKey(), 1, 33));
+			BigInteger y = new BigInteger(1, Arrays.copyOfRange(txInput.getPublicKey(), 33, 65));
+			ECPoint ecPoint = ecParameters.getCurve().createPoint(x, y);
+
+			ECPublicKeySpec keySpec = new ECPublicKeySpec(ecPoint, ecParameters);
+			PublicKey publicKey = keyFactory.generatePublic(keySpec);
+			ecdsaVerify.initVerify(publicKey);
+			ecdsaVerify.update(txCopy.getTransactionId());
+			if (!ecdsaVerify.verify(txInput.getSignature())) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 
